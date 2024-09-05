8e610092e957cd45d8256c129a28ac1aad516bc8
chongyang00 <ychong141@gmail.com>
2024-09-05 12:18:05 -0700

    Add files via upload

---
+++ FileIO.java
@@ -0,0 +1,30 @@
+//Chong Yang 9/5/24
+//FileIO.java
+
+import java.io.*;
+
+
+public class FileIO {
+    public static void main(String[] args) {
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write("Writing to a file.");
+            writer.close();
+
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+            String line;
+            while((line = reader.readLine()) !=null)
+                System.out.println(reader.readLine());
+            reader.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
